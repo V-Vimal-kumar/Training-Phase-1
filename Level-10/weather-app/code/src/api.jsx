2b4b409615acb2a2d8299792e23a03bba7c25220
VIMAL KUMAR V <127484073+V-Vimal-kumar@users.noreply.github.com>
2025-04-08 09:21:38 +0530

    Add files via upload

---
+++ Level-10/weather-app/code/src/api.jsx
@@ -0,0 +1,12 @@
+export const geoAPI = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '69f7f6f35amsh6e34088bed7be6bp1a0b9djsn40651e40f970',// 69f7f6f35amsh6e34088bed7be6bp1a0b9djsn40651e40f970
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export  const WEATHER_API_URL="https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY="93fe7cacda18561da84bf27c5106f34e"
